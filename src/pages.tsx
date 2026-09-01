9173a997647be2baf9be7923dcfae491cda8ab9c
Harris Alexandre <50559815+harrisalexandre@users.noreply.github.com>
2026-09-01 13:57:34 -0300

    fix: export shared primitives from pages barrel

--- src/pages.tsx
+++ src/pages.tsx
@@ -1,3 +1,3 @@
-export { Metric, AdminDashboard, AdminCompanies, NewCompany, CompanyDetail, Permissions, AdminUsers, AdminMessages, AdminPlaceholder } from './pages/admin'
-export { Empty, TenantPage } from './pages/shared'
+export { Metric, AdminPlaceholder, Empty, TenantPage, getSupabase, zonedParts, localDateKey, localDateTimeToUtc, utcToLocalInput, formatZoned, BR_TIMEZONE } from './pages/shared'
+export { AdminDashboard, AdminCompanies, NewCompany, CompanyDetail, Permissions, AdminUsers, AdminMessages } from './pages/admin'
 export { PatientsPage, ProfessionalsPage, AgendaPage, WhatsAppPage, AutomationsPage, TenantSettingsPage, TenantDashboard } from './pages/tenant'
